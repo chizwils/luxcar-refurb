c4236e29bf75254f05aab8bb81ce2f29b90d4f58
Chinedu Chizaram <c.chizaram@gmail.com>
2017-08-25 20:18:11 -0500

    added categories to hompage after sliders

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.dropdown-button').dropdown();
     $(".button-collapse").sideNav();
+   // $('.carousel.carousel-slider').carousel({fullWidth:true,indicators: true});
+    $('.slider').slider({indicators: false});
 
 })
