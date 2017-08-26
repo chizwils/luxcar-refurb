3b6bdd9e49b472f06a77013681748e4b2abdd32e
Chinedu Chizaram <c.chizaram@gmail.com>
2017-08-26 14:17:20 -0500

    Added an all vehocle list and a scroll plugin in homepage.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $('.dropdown-button').dropdown();
     $(".button-collapse").sideNav();
    // $('.carousel.carousel-slider').carousel({fullWidth:true,indicators: true});
-    $('.slider').slider({indicators: false});
-
+    $('.slider').slider({indicators: false, height: 700});
+    $('select').material_select();
+    var options=[
+        {selector: '.desc', offset: 100, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+       {selector: '.ult', offset: 400, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } }
+    ];
+    Materialize.scrollFire(options);
 })
